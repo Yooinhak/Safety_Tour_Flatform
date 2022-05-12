1b1b9133d3a69d826e56dc5c66d0a0fe8ccff460
Yooinhak <syu3236@gmail.com>
2022-05-13 00:38:10 +0900

    Revert "ios update"
    This reverts commit 3dba19df0832e25505a50c15dab5a252bb157f17.

--- src/screens/Service/GeoLocationAPI.js
+++ src/screens/Service/GeoLocationAPI.js
@@ -2,6 +2,36 @@ import React, {useState} from 'react'
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native'
 import Geolocation from '@react-native-community/geolocation'
 
+const KAKAO_API_KEY = '87bb3dde6f868e35e0a4f64884ba2c87'
+
+function GetAddress(x, y) {
+  axios
+    .get(
+      `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${x}&y=${y}`,
+      {
+        headers: {
+          Authorization: 'KakaoAK ' + KAKAO_API_KEY,
+        },
+      },
+    )
+    .then(response => {
+      response.data.documents.map(doc => {
+        if (doc.region_type === 'B') {
+          setLocCode(doc.code)
+        }
+      })
+    })
+    .catch(
+      e => {
+        console.log(e)
+        return 4571040024
+      },
+      [x, y],
+    )
+
+  return locCode
+}
+
 const GeoLocationAPI = ({}) => {
   const [latitude, setLatitude] = useState(null)
   const [longitude, setLongitude] = useState(null)
